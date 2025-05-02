2713844a522e106d0ae84bf673e946d614530687
Tilak Rana <114658444+tilak-rana@users.noreply.github.com>
2025-05-02 16:28:41 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,5 @@
-export const USER_API_END_POINT="https://getjob-backend-w1pq.onrender.com/api/v1/user";
-export const JOB_API_END_POINT="https://getjob-backend-w1pq.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://getjob-backend-w1pq.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://getjob-backend-w1pq.onrender.com/api/v1/company";
+export const BASE_URL="https://getjob-backend-w1pq.onrender.com";
+export const USER_API_END_POINT=`${BASE_URL}/api/v1/user`;
+export const JOB_API_END_POINT=`${BASE_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT=`${BASE_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT=`${BASE_URL}/api/v1/company`;
